8ad56f9dca403913e8c742dcb4777940e430d086
andron13 <andron13@gmail.com>
2025-07-29 10:34:33 +0200

    feat: theme toggler

--- src/enteties/dummy-sections/features-section/features-section.tsx
+++ src/enteties/dummy-sections/features-section/features-section.tsx
@@ -1,12 +1,13 @@
-import { Sun } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 
 export const FeaturesSection = () => (
   <section className="mb-16">
     <h2 className="mb-8 text-center text-3xl font-bold">Особенности</h2>
     <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
       <div className="rounded-xl border border-gray-200 p-6 transition-shadow hover:shadow-lg dark:border-gray-700">
-        <div className="mb-4 flex h-12 w-12 items-center justify-center rounded-lg bg-blue-100 dark:bg-blue-900">
+        <div className="mb-4 flex h-12 w-fit items-center gap-6 rounded-lg bg-blue-100 dark:bg-blue-900">
           <Sun className="h-6 w-6 text-blue-600 dark:text-blue-400" />
+          <Moon className="h-6 w-6 text-blue-600 dark:text-blue-400" />
         </div>
         <h3 className="mb-2 text-xl font-semibold">Темная/Светлая тема</h3>
         <p className="text-gray-600 dark:text-gray-300">
